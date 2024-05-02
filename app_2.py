c3f2b972a829e8670e60db53c0cef260304f69b5
MatheusRiosBarcelos <168184382+MatheusRiosBarcelos@users.noreply.github.com>
2024-05-02 11:18:58 -0300

    Add files via upload

--- app_2.py
+++ app_2.py
@@ -14,16 +14,24 @@ ordens.loc[:, 'Datetime_ini'] = pd.to_datetime(ordens['data_ini'] + ' ' + ordens
 ordens.loc[:,'Datetime_fim'] = pd.to_datetime(ordens['data_fim'] + ' ' + ordens['hora_fim'], format = 'mixed', errors='coerce')
 ordens.loc[:, 'delta_time_seconds'] = (ordens['Datetime_fim'] - ordens['Datetime_ini']).dt.total_seconds()
 ordens.loc[:, 'delta_time_hours'] = ordens['delta_time_seconds'] / 3600
+ordens.loc[:,'delta_time_min'] = ordens['delta_time_seconds']/60
 
-estacao = st.sidebar.selectbox("Estação", ordens["estacao"].sort_values().unique(), index= 0,placeholder ='Escolha uma opção')
+col6, col7, col8 = st.columns(3)
+with col6:
+    estacao = st.selectbox("Estação", ordens["estacao"].sort_values().unique(), index= 0,placeholder ='Escolha uma opção')
+
 st.header(f"Análise Hora de Trabalho Mensal {estacao}")
 
 ordens["data_ini"] = pd.to_datetime(ordens["data_ini"], format = 'mixed', errors='coerce')
 ordens=ordens.sort_values("data_ini")
 ordens["Ano"] = ordens["data_ini"].dt.year.astype('Int64') 
 ordens["Mes"] = ordens["data_ini"].dt.month.astype('Int64')
-target_year = st.sidebar.selectbox("Ano", ordens["Ano"].sort_values().unique(), index= 1 ,placeholder ='Escolha uma opção')
-target_month = st.sidebar.selectbox("Mês", ordens["Mes"].sort_values().unique(), index= 0,placeholder ='Escolha uma opção')
+
+with col8:
+    target_year = st.selectbox("Ano", ordens["Ano"].sort_values().unique(), index= 1 ,placeholder ='Escolha uma opção')
+    
+with col7:
+    target_month = st.selectbox("Mês", ordens["Mes"].sort_values().unique(), index= 0,placeholder ='Escolha uma opção')
 
 new_df = ordens[ordens['estacao'] == estacao]
 df_filtrado = new_df[new_df['Datetime_ini'].dt.month == target_month]
@@ -38,7 +46,9 @@ col1.metric(f"Total de Horas da Máquina em {target_month}-{target_year}", f"{to
 col2.metric('Eficiência (%)', f'{percent_horas}%', f'{delta_1}%')
 col3.metric("Média", f"{media}H")
 
-target_pv = st.sidebar.selectbox("PV", pedidos["pedido"].sort_values().unique(),placeholder ='Escolha uma opção')
+col9,col10,col11 = st.columns(3)
+with col9:
+    target_pv = st.selectbox("PV", pedidos["pedido"].sort_values().unique(),placeholder ='Escolha uma opção')
 st.header(f'Análise Hora de Trabalho PV {target_pv}')
 
 col4, col5 = st.columns(2)
@@ -53,17 +63,16 @@ descricao = descricao.iloc[0,14]
 pedido = pedidos[pedidos['pedido'] == target_pv]
 filtro_df = pedido['ordem']
 ordem = ordens[ordens['ordem'].isin(filtro_df)]
-ordem['delta_time_min'] = ordem['delta_time_hours']*60
-soma_por_estacao = ordem.groupby('estacao')['delta_time_min'].sum().reset_index().round(2)
-soma_por_estacao.rename(columns={'delta_time_min': 'Tempo de uso (min)'}, inplace=True)
+soma_por_estacao = ordem.groupby('estacao')['delta_time_hours'].sum().reset_index().round(2)
+soma_por_estacao.rename(columns={'delta_time_hours': 'Tempo de uso (H)'}, inplace=True)
 soma_por_estacao.rename(columns={'estacao': 'Estação de Trabalho'}, inplace=True)
-total_de_horas_pedido = round(ordem['delta_time_min'].sum(),2)
+total_de_horas_pedido = round(ordem['delta_time_hours'].sum(),2)
 
-fig = px.pie(soma_por_estacao, values='Tempo de uso (min)', names='Estação de Trabalho', title='Proporção de Tempo de Uso por Máquina em Cada Pedido', width=800, height=500)
+fig = px.pie(soma_por_estacao, values='Tempo de uso (H)', names='Estação de Trabalho', title='Proporção de Tempo de Uso por Máquina em Cada Pedido', width=800, height=500)
 fig.update_layout(title_yref='container',title_xanchor = 'center',title_x = 0.43, title_y = 0.95, legend=dict(font=dict(size=18)),font=dict(size=20), title_font=dict(size=20))
 col4.plotly_chart(fig, use_container_width=True)
 
-nova_linha = {'Estação de Trabalho': 'Total', 'Tempo de uso (min)': total_de_horas_pedido}
+nova_linha = {'Estação de Trabalho': 'Total', 'Tempo de uso (H)': total_de_horas_pedido}
 soma_por_estacao = pd.concat([soma_por_estacao, pd.DataFrame([nova_linha])], ignore_index=True)
 with col5:
     st.markdown(f"<h1 style='font-size: 20px;'>Tabela de Horas por Estação no PV {target_pv}</h1>", unsafe_allow_html=True)
@@ -71,11 +80,3 @@ with col5:
 col5.dataframe(soma_por_estacao, height= 250, width= 500,hide_index=True)
 
 st.markdown(f"<h1 style='text-align: left;'>{descricao}</h1>", unsafe_allow_html=True)
-
-
-
-
-
-
-
-
