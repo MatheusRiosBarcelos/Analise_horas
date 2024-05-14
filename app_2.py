eb93768f3a862caf6576bb45a86d0ba5b9041b70
MatheusRiosBarcelos <168184382+MatheusRiosBarcelos@users.noreply.github.com>
2024-05-14 13:13:00 -0300

    Add files via upload

--- app_2.py
+++ app_2.py
@@ -18,45 +18,59 @@ ordens.loc[:, 'delta_time_hours'] = ordens['delta_time_seconds'] / 3600
 ordens.loc[:,'delta_time_min'] = ordens['delta_time_seconds']/60
 st.image('logo.png', width= 150)
 
+
 tab1, tab2, tab3 = st.tabs(["ANÁLISE HORA DE TRABALHO MENSAL", "ANÁLISE HORA DE TRABALHO POR PV", "MÉDIA POR PEÇA"])
 with tab1:
     col6, col7, col8 = st.columns(3)
     with col6:
         estacao = st.selectbox("Estação", ordens["estacao"].sort_values().unique(), index= 0,placeholder ='Escolha uma opção')
     ordens["data_ini"] = pd.to_datetime(ordens["data_ini"], format = 'mixed', errors='coerce')
+    ordens["data_fim"] = pd.to_datetime(ordens["data_fim"], format = 'mixed', errors='coerce')
     ordens=ordens.sort_values("data_ini")
     ordens["Ano"] = ordens["data_ini"].dt.year.astype('Int64') 
     ordens["Mes"] = ordens["data_ini"].dt.month.astype('Int64')
+    ordens["Dia_ini"] = ordens["data_ini"].dt.day.astype('Int64') 
+    ordens["Dia_fim"] = ordens["data_fim"].dt.day.astype('Int64')
     with col7:
         target_month = st.selectbox("Mês", ordens["Mes"].sort_values().unique(), index= 0,placeholder ='Escolha uma opção')
     with col8:
         target_year = st.selectbox("Ano", ordens["Ano"].sort_values().unique(), index= 1 ,placeholder ='Escolha uma opção')
     
     new_df = ordens[ordens['estacao'] == estacao]
-    df_filtrado = new_df[new_df['Datetime_ini'].dt.month == target_month]
-    df_filtrado_2 = df_filtrado[df_filtrado['Datetime_ini'].dt.year == target_year]
-    hora_esperada_de_trabalho = 160
+    
+    df_filtrado_year = new_df[new_df['Datetime_ini'].dt.year == target_year]
+    df_filtrado = df_filtrado_year[df_filtrado_year['Datetime_ini'].dt.month == target_month]
+    hora_esperada_de_trabalho = 200
     num_entries = df_filtrado.shape[0]
-    total_de_horas = round(df_filtrado_2['delta_time_hours'].sum(), 1)
+    df_filtrado['delta_dia'] = df_filtrado['Dia_fim'] - df_filtrado['Dia_ini']
+    
+    if (df_filtrado['delta_dia'] != 0).any():
+        df_filtrado['delta_time_hours'] = df_filtrado['delta_time_hours'] - (df_filtrado['delta_dia']*14) 
+    
+    total_de_horas = round(df_filtrado['delta_time_hours'].sum(),1)
     percent_horas = round((total_de_horas/hora_esperada_de_trabalho) * 100, 1)
+    
     media = round(total_de_horas/num_entries,1)
+    
     delta_1 = round(percent_horas - 100, 1)
     
     col1, col2, col3 = st.columns(3)
-    col1.metric(f"Total de Horas da Máquina em {target_month}-{target_year}", f"{total_de_horas}H", f'{round(total_de_horas-160,1)}H')
+    col1.metric(f"Total de Horas da Máquina em {target_month}-{target_year}", f"{total_de_horas}H", f'{round(total_de_horas-200,1)}H')
     col2.metric('Eficiência (%)', f'{percent_horas}%', f'{delta_1}%')
     col3.metric("Média", f"{media}H")
    
     col11,col12 = st.columns([0.8,0.2])
+    df_filtrado_year['delta_dia'] = df_filtrado_year['Dia_fim'] - df_filtrado_year['Dia_ini']
+    if (df_filtrado_year['delta_dia'] != 0).any():
+        df_filtrado_year['delta_time_hours'] = df_filtrado_year['delta_time_hours'] - (df_filtrado_year['delta_dia']*14) 
     
-    new_df_ano = new_df[new_df['Datetime_ini'].dt.year == target_year]
-    ordem_2 = new_df_ano.groupby(['estacao', new_df_ano['Datetime_ini'].dt.month, 'nome_func'])['delta_time_hours'].sum().reset_index().round(2)
+    ordem_2 = df_filtrado_year.groupby(['estacao', df_filtrado_year['Datetime_ini'].dt.month])['delta_time_hours'].sum().reset_index().round(2)
     ordem_2.rename(columns = {'delta_time_hours':'Tempo de uso total (H)'}, inplace = True)
     ordem_2.rename(columns = {'Datetime_ini': 'Mês'}, inplace = True)
     
-    fig2 = px.bar(ordem_2, x = 'Mês', y = round((ordem_2['Tempo de uso total (H)']/hora_esperada_de_trabalho)*100,2),color='nome_func',title= 'Eficiência Mensal',text_auto='.2s', width=1300)
+    fig2 = px.bar(ordem_2, x = 'Mês', y = round((ordem_2['Tempo de uso total (H)']/hora_esperada_de_trabalho)*100,2),title= 'Eficiência Mensal',text_auto='.2s', width=1300)
     fig2.update_traces(textfont_size=16, textangle=0, textposition="outside", cliponaxis=False)
-    fig2.update_layout(yaxis_title = 'Eficiência (%)',legend_title_text = 'Colaborador', title_x = 0.5, title_y = 0.95,title_xanchor = 'center')
+    fig2.update_layout(yaxis_title = 'Eficiência (%)', title_x = 0.5, title_y = 0.95,title_xanchor = 'center')
     fig2.update_xaxes(tickvals=list(range(len(ordem_2)+1)))
     col11.plotly_chart(fig2)
 
